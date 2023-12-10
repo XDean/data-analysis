03a5f0a83a58146808d1c01513bbb742ba63bbfa
Dean Xu <373216024@qq.com>
2023-12-10 11:37:17 +0800

    init MenuBar

---
+++ src/components/MenuBar.tsx
@@ -0,0 +1,33 @@
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar';
+import Link from 'next/link';
+
+export const MenuBar = () => {
+  return (
+    <Menubar>
+      <div className={'px-2 border-primary border rounded text-xl content-end'}>
+        Data Analysis
+      </div>
+      <MenubarMenu>
+        <MenubarTrigger>文件</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            创建 分析面板...
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>关于</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            <Link href={'https://xdean.cn'} target={'_blank'}>
+              访问 xdean.cn ...
+            </Link>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <div className={'px-2 bg-primary rounded text-primary-foreground text-xl content-end'}>
+        XDean
+      </div>
+    </Menubar>
+  );
+};
